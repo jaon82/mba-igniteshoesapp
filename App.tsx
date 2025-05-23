a1faf0c699b423d8da63195b81c34079af8b816b
Jean Nunes <jean.nunes@uzzipay.com>
2025-05-23 18:08:35 -0400

    Run expo prebuild

--- App.tsx
+++ App.tsx
@@ -1,16 +1,31 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "react-native";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Loading } from "./src/components/Loading";
+import { THEME } from "./src/theme";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { useEffect } from "react";
+import { OneSignal } from "react-native-onesignal";
+import { CartContextProvider } from "./src/contexts/CartContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  // Initialize OneSignal in useEffect to ensure it runs only once
+  useEffect(() => {
+    // Initialize with your OneSignal App ID
+    OneSignal.initialize("136846cb-2161-43eb-a09d-59805b61fd03");
+    // Use this method to prompt for push notifications.
+    // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
+    //OneSignal.Notifications.requestPermission(false);
+  }, []); // Ensure this only runs once on app mount
 
   return (
     <NativeBaseProvider theme={THEME}>
